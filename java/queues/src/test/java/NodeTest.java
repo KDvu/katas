13473e39b5d319466e19fbd8f6cb9110ab1a5a71
Kenny Vu <k.vu.zerosoft@gmail.com>
2018-09-03 11:35:06 +0100

    [Java/queues] Clean up

--- java/queues/src/test/java/NodeTest.java
+++ java/queues/src/test/java/NodeTest.java
@@ -4,7 +4,6 @@ import static org.junit.jupiter.api.Assertions.assertSame;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Nested;
-import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.TestInfo;
 import org.junit.jupiter.api.BeforeEach;
